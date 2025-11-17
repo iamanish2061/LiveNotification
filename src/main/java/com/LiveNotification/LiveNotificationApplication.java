ed1c4b3a9a9f6610f6026a5c6e40b35488410221
AnishShrestha <anishnitro5@gmail.com>
2025-11-17 17:47:56 +0545

    Getting Started

---
+++ src/main/java/com/LiveNotification/LiveNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.LiveNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LiveNotification/LiveNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LiveNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
