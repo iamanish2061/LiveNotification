a339c0d384f72078204cb4101df929de084004f3
AnishShrestha <anishnitro5@gmail.com>
2025-11-22 16:02:05 +0545

    update

--- src/main/java/com/LiveNotification/kafka/KafkaConsumer.java
+++ src/main/java/com/LiveNotification/kafka/KafkaConsumer.java
@@ -2,17 +2,22 @@ package com.LiveNotification.kafka;
 
 import com.LiveNotification.entity.Notification;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
 
+    private final SimpMessagingTemplate template;
+
+    public KafkaConsumer(SimpMessagingTemplate template){
+        this.template = template;
+    }
+
     @KafkaListener(topics = "thecutlab",
     groupId = "notification-group")
-    public void consume(Notification notification){
-        System.out.println("Received Message" + notification.getMessage());
-
-        //forward to web socket
+    public void listen(Notification notification){
+        template.convertAndSend("/topic/notifications", notification);
     }
 
 }
